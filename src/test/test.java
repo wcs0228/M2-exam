7fa862062ae8f7ecfe66e9da46cc6040e7cef6ee
wcs0228 <chse0226>
2019-11-23 16:03:28 +0800

    ALL

--- src/test/test.java
+++ src/test/test.java
@@ -5,19 +5,15 @@ public class test {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int sum=0;
-        int a;
-        for(int k =1;k<=n;k++)
-        {
-            a=1;
-            for (int i = 1; i <= k; i++)
-            {
 
-               a =a*2;
+        for(int i =1;i<=n;i++)
+        {
+            if(n%i == 0)
+            {
+                System.out.println(i);
             }
-            sum=sum+a;
         }
-        System.out.println(sum);
+
 
     }
 }
